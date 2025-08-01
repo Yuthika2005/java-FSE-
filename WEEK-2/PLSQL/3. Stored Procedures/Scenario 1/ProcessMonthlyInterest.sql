533c12b88eb79efe6dcd76c17191767ae6be621c
Yuthika A <yuthikatrt@gmail.com>
2025-08-01 08:41:11 +0530

    Delete WEEK-2 directory

--- WEEK-2/PLSQL/3. Stored Procedures/Scenario 1/ProcessMonthlyInterest.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
-BEGIN
-    UPDATE Accounts
-    SET Balance = Balance + (Balance * 0.01) -- 1% interest rate
-    WHERE AccountType = 'Savings';
-
-    COMMIT;
-END ProcessMonthlyInterest;
-/
--- WEEK-2/PLSQL/4. Functions/Scenario 1/CalculateAge.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION CalculateAge(
-    p_dob IN DATE
-) RETURN NUMBER IS
-BEGIN
-    RETURN FLOOR((SYSDATE - p_dob) / 365);
-END CalculateAge;
-/
